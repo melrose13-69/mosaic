315a2512f40e1c5cd8a4c71cee275d58eaf384d2
poltoratchi <vpoltoratchi@noction.com>
2024-02-22 23:05:53 +0200

    added prefixes color for images and fixed some fnc

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import {MosaicMatrix} from "./modules/MosaicMatrix";
 
 ;(async () => {
     try {
-        console.time('time')
         const mosaic = new MosaicMatrix(resolve(__dirname, '../src/images/example.jpg'), [11, 12], [7, 10])
 
         const image = await mosaic.generateMosaicImage('disco')
@@ -12,9 +11,8 @@ import {MosaicMatrix} from "./modules/MosaicMatrix";
         const base64Image = image.split(';base64,').pop() as string
 
         writeFileSync(resolve(__dirname, '../src/images/disco.png'), base64Image, {encoding: 'base64'})
-        console.timeEnd('time')
 
-        // const doc = await mosaic.generateInstructions(base64Image, 'ocean', 'output.pdf')
+        const doc = await mosaic.generateInstructions(base64Image, 'ocean', 'output.pdf')
 
     } catch (e) {
         throw new Error(e)
